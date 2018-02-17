9d51b09c651cafa2172a9d28100e1318a3c5de61
jurajmajerik <juro.majerik@gmail.com>
2018-02-17 22:09:43 +0700

    add proptypes, refactor functionalities into modules

--- src/App.js
+++ src/App.js
@@ -1,41 +1,41 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import {TodoForm} from './components/todo/TodoForm';
+import {TodoForm, TodoList} from './components/todo'
 
 class App extends Component {
   constructor() {
-    super();
+    super()
     this.state = {
       todos: [
-        {id: 1, name: 'Do one', isComplete: true},
-        {id: 2, name: 'Do two', isComplete: false},
-        {id: 3, name: 'Do three', isComplete: false}
+        {id: 1, name: 'Learn JSX', isComplete: true},
+        {id: 2, name: 'Build an Awesome App', isComplete: false},
+        {id: 3, name: 'Ship It', isComplete: false}
       ],
       currentTodo: ''
-    };
-    this.handleInputChange = this.handleInputChange.bind(this);
+    }
+    this.handleInputChange = this.handleInputChange.bind(this)
   }
-  handleInputChange(event) {
-    this.setState({ currentTodo: event.target.value });
+
+  handleInputChange (evt) {
+    this.setState({
+      currentTodo: evt.target.value
+    })
   }
+
   render() {
     return (
       <div className="App">
-        <h2>React Todos</h2>
-      <div className="Todo-App">
-        <TodoForm
-          handleInputChange={this.handleInputChange}
-          currentTodo={this.state.currentTodo}/>
-      </div>
-      <div className="Todo-List">
-      <ul>
-        {this.state.todos.map(todo =>
-        <li key={todo.id}>
-          <input type="checkbox" defaultChecked={todo.isComplete}/>{todo.name}
-        </li>)}
-      </ul>
-      </div>
+        <div className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h2>React Todos</h2>
+        </div>
+        <div className="Todo-App">
+          <TodoForm handleInputChange={this.handleInputChange}
+            currentTodo={this.state.currentTodo}/>
+          <TodoList todos={this.state.todos}/>
+
+        </div>
       </div>
     );
   }
--- src/components/todo/TodoForm.js
+++ src/components/todo/TodoForm.js
@@ -1,9 +1,14 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 
 export const TodoForm = (props) => (
   <form>
-    <input
-      type="text"
-      value={props.currentTodo}
-      onChange={props.handleInputChange}/>
-  </form>);
+    <input type="text"
+      onChange={props.handleInputChange}
+      value={props.currentTodo}/>
+  </form>)
+
+TodoForm.propTypes = {
+  currentTodo: PropTypes.string.isRequired,
+  handleInputChange: PropTypes.func.isRequired
+}
